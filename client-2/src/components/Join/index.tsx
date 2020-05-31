2bf655475154561278b08ee31519f9f80cbea518
ethanpmullen <65503442+ethanpmullen@users.noreply.github.com>
2020-05-31 12:54:08 -0400

    Fixed join

--- client-2/src/components/Join/index.tsx
+++ client-2/src/components/Join/index.tsx
@@ -11,28 +11,34 @@ function Join() {
       <JoinWrapper>
         <Title>Join</Title>
         <div>
-          <div>
-            <input
-              placeholder="Name"
-              type="text"
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-          <div>
-            <input
+          <Input
+            placeholder="Name"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+          <Layer>
+            <Input
               placeholder="Room"
               type="text"
               onChange={(event) => setRoom(event.target.value)}
             />
-          </div>
-          <Link
-            onClick={(event: any) =>
-              !name || !room ? event.preventDefault() : null
-            }
-            to={`/chat?name=${name}&room=${room}`}
-          >
-            <button type="submit">Sign in!</button>
-          </Link>
+            <LinkStyle
+              onClick={(event: any) =>
+                !name || !room ? event.preventDefault() : null
+              }
+              to={`/chat?name=${name}&room=${room}`}
+            >
+              <Button type="submit">Go!</Button>
+            </LinkStyle>
+          </Layer>
+          <Layer>
+            <LinkStyle
+              onClick={(event: any) => (!name ? event.preventDefault() : null)}
+              to={`/chat?name=${name}&room=${name}`}
+            >
+              <Button type="submit">Create new room with name</Button>
+            </LinkStyle>
+          </Layer>
         </div>
       </JoinWrapper>
     </Container>
@@ -63,4 +69,40 @@ const JoinWrapper = styled.div`
 const Title = styled.h1`
   margin-bottom: 40px;
   position: relative;
+`;
+
+const Layer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  display: flex;
+  outline: none;
+  padding: 12px;
+  border-radius: 4px;
+  border: none;
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0;
+`;
+
+const Button = styled.button`
+  background-color: darkslategray;
+  display: flex;
+  padding: 12px;
+  border-radius: 4px;
+  border: none;
+  font-weight: bold;
+  font-size: 14px;
+  margin: 0;
+  margin-top: auto;
+  color: white;
+  height: 40px;
+`;
+
+const LinkStyle = styled(Link)`
+  color: white;
 `;
